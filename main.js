d51333eac84e743a2b6ae2063920bc8371a844cf
Ryan Hetherington <hetheringtonr2@comcast.net>
2021-08-16 16:05:42 -0700

    Made start menu

---
+++ main.js
@@ -0,0 +1,11 @@
+// gameBoard Factory Function
+// Make sure every important bit of code is inside the factory
+const gameBoard = (() => {
+    
+    modal = document.querySelector("#modal");
+    startBtn = document.querySelector("#start");
+
+    console.log("Gameboard online.");
+}); 
+
+gameBoard();
