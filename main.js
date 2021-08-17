79fd409de36d525451603ff6d69a7d471b789e5e
Ryan Hetherington <hetheringtonr2@comcast.net>
2021-08-17 15:24:40 -0700

    Added player logic

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-// gameBoard Factory Function
+// gameStart Factory Function
 // Make sure every important bit of code is inside the factory
-const gameBoard = (()=> {
+const gameStart = (()=> {
     let puzzleGrid = document.querySelector(".main-container");
     let modal = document.querySelector(".modal");
     let startBtn = document.querySelector("#start");
@@ -48,8 +48,54 @@ const gameBoard = (()=> {
     }
     return {startGameAI, startGamePlayer, resetGame};
 })(); 
-const gameStart = [gameBoard.startGameAI(), gameBoard.startGamePlayer()];
-const gameRestart = gameBoard.resetGame();
-//gameBoard.startGameAI();
-//gameBoard.startGamePlayer();
-//gameBoard.resetGame();
+const startYourEngines = [gameStart.startGameAI(), gameStart.startGamePlayer()];
+const gameRestart = gameStart.resetGame();
+
+const gameBoardPlayer = (()=> {
+    const friendshipEnded = (name, symbol, computer, turn) => {
+        return {name, symbol, computer, turn};
+    };
+    const player1 = friendshipEnded('player1', 'X', false, true);
+    const player2 = friendshipEnded('player2', 'O', false, false);
+    let gameArray = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [6, 4, 2]
+    ];
+    let winnerArray = [];
+    let winner = null;
+    let turns = 0;
+    let board = [];
+    const playerTurns = (()=> {
+        let block = document.querySelectorAll('.block');
+        block.forEach( block => {
+            block.addEventListener('click', (event)=> {
+               //Player One Turn
+               if(player1.turn == true && winner == null && event.target.textContent == '') {
+                    board[event.target.id] = player1.symbol;
+                    block.textContent = player1.symbol;
+                    block.style.color = '#2de2e6';
+                    player1.turn = false;
+                    player2.turn = true;
+                    console.log(board);
+                    console.log(player1);
+                    console.log(player2);
+               } else {
+                   board[event.target.id] = player2.symbol;
+                   block.textContent = player2.symbol;
+                   block.style.color = '#2de2e6';
+                   player1.turn = true;
+                   player2.turn = false;
+                   console.log(board);
+                   console.log(player2);
+               };
+            });
+        });
+    });
+    return {playerTurns};
+})();
+const gamePlayerStart = gameBoardPlayer.playerTurns();
