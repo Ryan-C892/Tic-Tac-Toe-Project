da4361697b9c605eb5a4293c8a2446b3fc90fd43
Ryan Hetherington <hetheringtonr2@comcast.net>
2021-08-17 14:11:36 -0700

    Added start menu logic

--- main.js
+++ main.js
@@ -1,11 +1,55 @@
 // gameBoard Factory Function
 // Make sure every important bit of code is inside the factory
-const gameBoard = (() => {
-    
-    modal = document.querySelector("#modal");
-    startBtn = document.querySelector("#start");
-
-    console.log("Gameboard online.");
-}); 
-
-gameBoard();
+const gameBoard = (()=> {
+    let puzzleGrid = document.querySelector(".main-container");
+    let modal = document.querySelector(".modal");
+    let startBtn = document.querySelector("#start");
+    let buttons = document.querySelector(".buttons");
+    let playerSelect = document.querySelector(".player-select");
+    const playerName = (name) => {
+        return {name};
+    };
+    let player1;
+    let player2;
+    let resetBtn = document.querySelector("#reset");
+    const startGameAI = ()=> {
+        startBtn.addEventListener("click", ()=> {
+            buttons.classList.remove("invisible");
+        });
+        let computerBtn = document.querySelector("#computerBtn");
+        computerBtn.addEventListener("click", ()=> {
+            modal.classList.add("invisible");
+            puzzleGrid.classList.remove("invisible");
+        });        
+    }
+    const startGamePlayer = ()=> {
+        const form = document.getElementById("player-select");
+        function handleForm(event) { event.preventDefault(); } 
+        form.addEventListener('submit', handleForm);
+        let playerBtn = document.querySelector("#playerBtn");
+        let submit = document.querySelector("#submit");
+        playerBtn.addEventListener("click", ()=> {
+            playerSelect.classList.remove("invisible");
+            player1 = playerName(document.getElementById('player1').value);
+            player2 = playerName(document.getElementById('player2').value);
+        });
+        submit.addEventListener("click",()=> {
+            console.log("Start!");
+            modal.classList.add("invisible");
+            puzzleGrid.classList.remove("invisible");
+        });
+    }
+    const resetGame = ()=> {
+        resetBtn.addEventListener("click", ()=> {
+            console.log("reset");
+            modal.classList.remove("invisible");
+            puzzleGrid.classList.add("invisible");
+        });
+    }
+    return {startGameAI, startGamePlayer, resetGame};
+})(); 
+const gameStart = [gameBoard.startGameAI(), gameBoard.startGamePlayer()];
+const gameRestart = gameBoard.resetGame();
+//gameBoard.startGameAI();
+//gameBoard.startGamePlayer();
+//gameBoard.resetGame();
